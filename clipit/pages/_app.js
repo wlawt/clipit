6a413bb7c12bfdc8a3bff2cfa36d564fee931c41
wlawt <williamlaw.wtl@gmail.com>
2021-01-06 17:03:56 -0500

    update app name

--- clipit/pages/_app.js
+++ clipit/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function ClipIt({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default ClipIt
